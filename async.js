e347d19474c3a7288cd0c2640f729f52e8e52680
WBDiamond <360mlgnoscope420blazeit@gmail.com>
2017-11-27 02:45:00 +0500

    hello

--- async.js
+++ async.js
@@ -7,7 +7,36 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise((resolve) => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+        let resultArray = [];
+        let doneJobs = 0;
+
+        let wrappedJobs = jobs.map(job => () => new Promise(resolveJob => {
+            job().then(resolveJob);
+            setTimeout(resolveJob, timeout, new Error('Promise timeout'));
+        }));
+
+        let startQueue = wrappedJobs.slice(0, parallelNum);
+
+        startQueue.forEach((job, i) => {
+            job().then(handler.bind(null, i));
+        });
+
+        function handler(index, jobResult) {
+            resultArray[index] = jobResult;
+            if (jobs.length > ++doneJobs) {
+                wrappedJobs[doneJobs + parallelNum - 1]()
+                    .then(handler.bind(null, doneJobs + parallelNum - 1));
+            } else {
+                resolve(resultArray);
+            }
+        }
+
+    });
 }
